2ddb302cd4e1ec782a724a0cf48b1ffb4755486b
Adedolapo Adegoke <shayne_huey@Adedolapos-MacBook-Pro.local>
2024-05-26 11:48:44 -0400

    skills section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
